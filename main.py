4da61674f901bbb0a739ba2f78f3e1e9f027baf2
Asankhaya Sharma <codelion@users.noreply.github.com>
2024-03-30 08:13:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from jwt import algorithms
 from django.utils import formats
 from rsa import cli
 from requests import sessions
+import request
 
 if __name__ == '__main__':
     formats.get_format()
     algorithms.HMACAlgorithm.prepare_key()
     cli.VerifyOperation.perform_operation()
     sessions.SessionRedirectMixin.resolve_redirects()
+    session = requests.Session()
+    proxies = {
+        'http': 'http://test:pass@localhost:8080',
+        'https': 'http://test:pass@localhost:8090',
+    }
+    req = requests.Request('GET', url)
+    prep = req.prepare()
+    session.rebuild_proxies(prep, proxies)
